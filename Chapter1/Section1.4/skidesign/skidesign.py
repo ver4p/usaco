f920153c37ad1ce6639737b520a145390c362f4d
ver4p <vera.poechhacker@gmail.com>
2018-06-25 06:54:17 +0200

    comments for code 'skidesign.py' in section 1.4

--- Chapter1/Section1.4/skidesign/skidesign.py
+++ Chapter1/Section1.4/skidesign/skidesign.py
@@ -7,19 +7,22 @@ TASK: skidesign
 """
 
 with open("skidesign.in", "r") as f:
-    n = int(f.readline())
-    h = [int(x) for x in f.readlines()]
+    n = int(f.readline())                   #number of hills
+    h = [int(x) for x in f.readlines()]     #elevation of each hill
     
 h.sort()
-hmin = h[0]
-hmax = h[-1]
+hmin = h[0]                                 #lowest altitude
+hmax = h[-1]                                #highest altitude
 dmax = hmax-hmin
 
-def calculate_costs(hmin, hmax, h):
-    lower = [(x-hmin)**2 for x in h if x<hmin]
-    higher = [(x-hmax)**2 for x in h if x>hmax]
+#calculates costs to change hills in order to have them in the intervall [h1, h2]
+def calculate_costs(h1, h2, h):
+    lower = [(x-h1)**2 for x in h if x<h1]
+    higher = [(x-h2)**2 for x in h if x>h2]
     return sum(lower) + sum(higher)
 
+#computes costs to have hills in [hmin, hmin+17], [hmin+1, hmin-16],... [hmax-17, hmax]
+#and finds minimum
 o = min([calculate_costs(k, k+17, h) for k in range(hmin, hmax - 17)]) if hmax-hmin > 17 else 0
 
 with open("skidesign.out", "w") as f:
